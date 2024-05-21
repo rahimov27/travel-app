6d4d24b3d2bc811b5eb9bb92234f40d46ca763a0
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2024-05-21 05:33:54 +0200

    added search bar

--- lib/ui/firstPage.dart
+++ lib/ui/firstPage.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:travel_app/blocs/first_enter/first_enter_bloc.dart';
+import 'package:travel_app/blocs/selected_city/selected_city_bloc.dart';
+import 'package:travel_app/common_widgets/bottomWidget.dart';
 import 'package:travel_app/common_widgets/buttonWidget.dart';
 import 'package:travel_app/common_widgets/cardWidget.dart';
+import 'package:travel_app/common_widgets/cityWidget.dart';
 import 'package:travel_app/common_widgets/modalSheetWidget.dart';
 import 'package:travel_app/resources/AppFonts.dart';
+import 'package:travel_app/ui/selectedCityPage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -56,6 +62,190 @@ class _HomePageState extends State<HomePage> {
         const SnackBar(content: Text('Preferences not initialized')),
       );
     }
+  }
+
+  void _showModalBottomSheet(BuildContext context) {
+    showBarModalBottomSheet(
+      backgroundColor: const Color(0xff242529),
+      context: context,
+      builder: (context) => SizedBox(
+        width: double.infinity,
+        height: 700,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 45),
+              child: Container(
+                width: double.infinity,
+                height: 90,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: const Color(0xff3E3F43),
+                ),
+                child: Row(
+                  children: [
+                    const Padding(padding: EdgeInsets.only(left: 8)),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: SvgPicture.asset(
+                                    "assets/images/plane-2.svg"),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: SizedBox(
+                                  width: 290,
+                                  height: 42,
+                                  child: TextField(
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.allow(
+                                        RegExp(
+                                            r'[а-яА-Я\s]'), // Only allow Cyrillic characters and whitespace
+                                      ),
+                                    ],
+                                    decoration: const InputDecoration(
+                                      hintText: "Минск",
+                                      hintStyle: AppFonts.minsk,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  BlocProvider.of<SelectedCityBloc>(context)
+                                      .add(LoadSelectedCity());
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SelectedCityPage(),
+                                    ),
+                                  );
+                                },
+                                child: SvgPicture.asset(
+                                  color: Colors.white,
+                                  "assets/images/search.svg",
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: SizedBox(
+                                  height: 32,
+                                  width: 290,
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: TextField(
+                                      style: AppFonts.minsk,
+                                      inputFormatters: [
+                                        FilteringTextInputFormatter.allow(
+                                          RegExp(
+                                              r'[а-яА-Я\s]'), // Only allow Cyrillic characters and whitespace
+                                        ),
+                                      ],
+                                      decoration: const InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Куда - Турция",
+                                        hintStyle: AppFonts.turkish,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: SvgPicture.asset(
+                                  width: 20,
+                                  "assets/images/close.svg",
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                BottomCardWidget(
+                  text: "Сложный маршрут",
+                  icon: "assets/images/marshrut.svg",
+                  color: Color(0xff3A633B),
+                ),
+                BottomCardWidget(
+                  color: Color(0xff2261BC),
+                  icon: "assets/images/world.svg",
+                  text: "Куда угодно",
+                ),
+                BottomCardWidget(
+                  color: Color(0xff00427D),
+                  icon: "assets/images/calendar.svg",
+                  text: "Выходные",
+                ),
+                BottomCardWidget(
+                  color: Color(0xffFF5E5E),
+                  icon: "assets/images/fire.svg",
+                  text: "Горячие билеты",
+                ),
+              ],
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 21.0, vertical: 30),
+              child: Container(
+                width: double.infinity,
+                height: 216,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: const Color(0xff2F3035),
+                ),
+                child: const Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          CityWidget(
+                            title: "Стамбул",
+                            image: "assets/images/turkey.png",
+                            subtitle: "Популярное направление",
+                          ),
+                          CityWidget(
+                            title: "Сочи",
+                            image: "assets/images/sochi.png",
+                            subtitle: "Популярное направление",
+                          ),
+                          CityWidget(
+                            title: "Пхукет",
+                            image: "assets/images/phuket.png",
+                            subtitle: "Популярное направление",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
@@ -116,6 +306,7 @@ class _HomePageState extends State<HomePage> {
                                   height: 42,
                                   child: TextField(
                                     controller: minskController,
+                                    style: AppFonts.minsk,
                                     inputFormatters: [
                                       FilteringTextInputFormatter.allow(
                                         RegExp(
@@ -136,22 +327,30 @@ class _HomePageState extends State<HomePage> {
                                   width: 290,
                                   child: Align(
                                     alignment: Alignment.centerLeft,
-                                    child: TextField(
-                                      controller: turkeyController,
-                                      inputFormatters: [
-                                        FilteringTextInputFormatter.allow(
-                                          RegExp(
-                                              r'[а-яА-Я\s]'), // Only allow Cyrillic characters and whitespace
-                                        ),
-                                      ],
-                                      decoration: const InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: "Куда - Турция",
-                                        hintStyle: AppFonts.turkish,
-                                      ),
-                                      onSubmitted: (value) {
-                                        _saveCity();
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        _showModalBottomSheet(context);
                                       },
+                                      child: AbsorbPointer(
+                                        child: TextField(
+                                          controller: turkeyController,
+                                          style: AppFonts.turkish,
+                                          inputFormatters: [
+                                            FilteringTextInputFormatter.allow(
+                                              RegExp(
+                                                  r'[а-яА-Я\s]'), // Only allow Cyrillic characters and whitespace
+                                            ),
+                                          ],
+                                          decoration: const InputDecoration(
+                                            border: InputBorder.none,
+                                            hintText: "Куда - Турция",
+                                            hintStyle: AppFonts.turkish,
+                                          ),
+                                          onSubmitted: (value) {
+                                            _saveCity();
+                                          },
+                                        ),
+                                      ),
                                     ),
                                   ),
                                 ),
@@ -191,14 +390,12 @@ class _HomePageState extends State<HomePage> {
                           // Determine the image based on the id
                           String imagePath;
                           if (id == 1) {
-                            imagePath =
-                                '/Users/r27/StudioProjects/travel_app/assets/images/girl.png';
+                            imagePath = 'assets/images/girl.png';
                           } else if (id == 2) {
                             imagePath =
-                                '/Users/r27/StudioProjects/travel_app/assets/images/socrat-i-lera-600x600.png';
+                                'assets/images/socrat-i-lera-600x600.png';
                           } else {
-                            imagePath =
-                                '/Users/r27/StudioProjects/travel_app/assets/images/samara.png';
+                            imagePath = 'assets/images/samara.png';
                           }
 
                           return Padding(
@@ -207,8 +404,7 @@ class _HomePageState extends State<HomePage> {
                               title: title,
                               price: price,
                               town: town,
-                              imagePath:
-                                  imagePath, // Pass the image path to the CardWidget
+                              imagePath: imagePath,
                             ),
                           );
                         },
